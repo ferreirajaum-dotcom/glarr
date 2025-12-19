106407236fc0753af3fcf10212587621cc89dfef
ferreirajaum-dotcom <ferreirajaum@icloud.com>
2025-12-19 11:54:24 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Mission from "./components/Mission";
 import Partners from "./components/Partners";
